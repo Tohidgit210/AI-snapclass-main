ea73d0dab5d05e2133161e26fac8d5086ee21d2b
Tohidgit210 <tohidtohid2357@gmail.com>
2026-09-19 18:21:48 +0530

    snapclass

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,55 +1,29 @@
 
-
-
 import streamlit as st
+
+
+
+
 
 def footer_home():
     logo_url = "https://tse1.mm.bing.net/th/id/OIP.eOED9H2dmVT3FMdInMGpSQHaHa?r=0&pid=Api&h=220&P=0"
-
+    
     st.markdown(f"""
-        <div style="
-            margin-top: 2rem;
-            display: flex;
-            gap: 8px;
-            justify-content: center;
-            align-items: center;
-        ">
-            <p style="
-                font-weight: bold;
-                color: white;
-                margin: 0;
-            ">
-                Created with ❤️ by SnapClass
-            </p>
-
-            <img src="{logo_url}"
-                 style="height: 25px; width: 25px;
-                        object-fit: contain;" />
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by snapclass </p>  
+        <img src='{logo_url}' style='max-height:25px' />
         </div>
-    """, unsafe_allow_html=True)
+                
+                """, unsafe_allow_html=True)
 
 
 def footer_dashboard():
     logo_url = "https://tse1.mm.bing.net/th/id/OIP.eOED9H2dmVT3FMdInMGpSQHaHa?r=0&pid=Api&h=220&P=0"
-
+    
     st.markdown(f"""
-        <div style="
-            margin-top: 2rem;
-            display: flex;
-            gap: 8px;
-            justify-content: center;
-            align-items: center;
-        ">
-            <p style="
-                font-weight: bold;
-                color: black;
-                margin: 0;
-            ">
-                Created with ❤️ by SnapClass
-            </p>
-
-            <img src="{logo_url}"
-                 style="height: 25px; width: 25px;
-                        object-fit: contain;" />
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by snapclass </p>  
+        <img src='{logo_url}' style='max-height:25px' />
         </div>
-    """, unsafe_allow_html=True)
+                
+                """, unsafe_allow_html=True)
